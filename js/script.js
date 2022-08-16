f5215b5aa47e473a5d3e227baa179d02417c6dcf
pouy4 <afsharipouya10@gmail.com>
2022-08-17 00:36:33 +0430

    updated

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var x = prompt("Enter first number : ");
+var y = prompt("Enter second number : ");
+
+alert(Number(x) + Number(y));
+alert(Number(x) - Number(y));
+alert(Number(x) * Number(y));
+alert(Number(x) / Number(y));
