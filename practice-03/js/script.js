5c99af3ca5a823a1dd040701aef0aa311ba7b501
pouy4 <afsharipouya10@gmail.com>
2022-08-17 16:54:38 +0430

    updated

--- practice-03/js/script.js
+++ practice-03/js/script.js
@@ -1,5 +1,5 @@
-var num1 = prompt("Enter number 1 : ");
-var num2 = prompt("Enter number 2 : ");
-var num3 = prompt("Enter number 3 : ");
-var average = (Number(num1) + Number(num2) + Number(num3)) / 3;
+var num1 = Number(prompt("Enter number 1 : "));
+var num2 = Number(prompt("Enter number 2 : "));
+var num3 = Number(prompt("Enter number 3 : "));
+var average = (num1 + num2 + num3) / 3;
 alert("Average is : " + average);
