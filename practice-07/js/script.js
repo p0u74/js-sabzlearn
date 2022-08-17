644d7b8af6b9be8a9919c1d7f2bd175463b2229d
pouy4 <afsharipouya10@gmail.com>
2022-08-17 18:28:04 +0430

    updated

--- practice-07/js/script.js
+++ practice-07/js/script.js
@@ -1,7 +1,9 @@
 var time = Number(prompt("زمان را به دقیقه وارد کنید : "));
 var timeHour = time / 60;
 
-if (time < 0) {
+if (isNaN(timeHour)) {
+  alert("زمان وارد شده صحیح نمیباشد.")
+} else if (time < 0) {
   alert("زمان وارد شده صحیح نمیباشد.");
 } else {
   alert(time + " دقیقه برابر است با " + timeHour + " ساعت.");
